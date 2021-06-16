e27ed30299952ca98b493ceef35d018e3e509b01
thecreatorpc101 <72680645+thecreatorpc@users.noreply.github.com>
2021-06-16 22:46:34 +0530

    Update max_in_an_array.c

--- max_in_an_array.c
+++ max_in_an_array.c
@@ -1,17 +1,9 @@
-//C program to find maximum in arr[] of size n
 #include <stdio.h>
-// C function to find maximum in arr[] of size n
-
 int maximum(int arr[], int n)
 {
 
     int i;
-    // Initialize maximum element
-
-    int max = arr[0];
-    // Traverse array elements from second and
-    // compare every element with current max  
-
+    int max = arr[0];  
     for (i = 1; i < n; i++)
     {
         if (arr[i] > max)
@@ -26,7 +18,6 @@ int maximum(int arr[], int n)
 int main()
 {
     int arr[5],i;
-    //Taking input from the user.
     printf("enter 5 numbers:");
     for (i=0;i<= sizeof(arr[5]);i++)
     {
