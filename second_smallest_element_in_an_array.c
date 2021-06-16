8faa17639300753c837caa310304ee2feadfe15b
thecreatorpc101 <72680645+thecreatorpc@users.noreply.github.com>
2021-06-17 00:22:21 +0530

    Update second_smallest_element_in_an_array.c

--- second_smallest_element_in_an_array.c
+++ second_smallest_element_in_an_array.c
@@ -29,3 +29,49 @@ main()
     }
     printf(" \nSecond smallest element is %d", secondsmallest);
 }
+
+
+
+
+
+
+
+
+
+
+// #include<stdio.h>
+// #include<conio.h>
+ 
+// void main()
+// {
+//     int a[50];
+//     int n,i,small,s_small;
+//     clrscr();
+     
+//     printf("\n Enter number of elements: ");
+//     scanf("%d",&n);
+     
+//     printf("\n Enter %d elements: ",n);
+//     for(i=0;i<n;i++)
+//     {
+//         scanf("%d",&a[i]);
+//     }
+     
+//     small=s_small=a[0];
+     
+//     for(i=1;i<n;i++)
+//     {
+//         if(small>a[i])
+//         {
+//             s_small=small;
+//             small=a[i];
+//         }
+//         else if(s_small>a[i] && a[i]!=small)
+//         {
+//             s_small=a[i];
+//         }
+//     }
+     
+//     printf("\n The Second Smallest Element in the given Array: %d", s_small);
+//     getch();
+// }
