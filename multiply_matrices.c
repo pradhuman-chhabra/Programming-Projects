88781844fdf3b0de795dfa36fc061108101a2e27
thecreatorpc101 <72680645+thecreatorpc@users.noreply.github.com>
2021-06-17 23:24:50 +0530

    Update multiply_matrices.c

--- multiply_matrices.c
+++ multiply_matrices.c
@@ -1,1 +1,41 @@
-
+#include <stdio.h>
+int main()
+{
+    int a[3][3], b[3][3],c[3][3], sum, k,i,j
+    ;
+    printf("enter the 9 number for the first matrix");
+    for (i = 0; i <= 2; i++)
+    {
+        for (j = 0; j <= 2; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    printf("enter the 9 number for the second matrix");
+    for (i = 0; i <= 2; i++)
+    {
+        for (j = 0; j <= 2; j++)
+        {
+            scanf("%d", &b[i][j]);
+        }
+    }
+    for (i = 0; i <= 2; i++)
+    {
+        for (j = 0; j <= 2; j++)
+        {
+            sum = 0;
+            for (k = 0; k <= 2; k++)
+                sum = sum + a[i][k] * b[k][j];
+            c[i][j] = sum;
+        }
+    }
+    printf("the following are the numbers in matrix c:");
+    for (i = 0; i <= 2; i++)
+    {
+        for (j = 0; j <= 2; j++)
+        {
+        printf("%d",c[i][j]);
+        }
+    }
+        return 0;
+}
